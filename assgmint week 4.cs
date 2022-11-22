3974c5ab495b730ed421984381eac4e4cc2eb1c0
YazanAlNobani <118618604+YazanAlNobani@users.noreply.github.com>
2022-11-22 11:40:09 +0300

    Add files via upload

---
+++ assgmint week 4.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnclick_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog()==System.Windows.Forms.DialogResult.OK)
+            {
+                Image image = Image.FromFile(ofd.FileName);
+                pictureBox1.Image  = image;
+}
+            }
+    }
+}
